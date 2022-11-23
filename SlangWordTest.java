40436788da0b418f8de0e22eeacb9c57863551c7
sonhuynh.08092002@gmail.com <jinho>
2022-11-23 07:38:09 +0700

    xoa 1 so code va file du thua

--- SlangWordTest.java
+++ SlangWordTest.java
@@ -1,16 +1,7 @@
 
-import java.util.ArrayList;
-import java.util.HashMap;
-
-/**
- *
- * @author Admin
- */
-
-
 public class SlangWordTest {
     public static void main(String[] args) {
-        SlangWordView v = new SlangWordView();
+        new SlangWordView();
 
     }
 }
